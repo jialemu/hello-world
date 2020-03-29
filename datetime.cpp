e40e4cf344546e9b952adb24786018850f5ff6dd
jialemu <1797787667@qq.com>
2020-03-29 23:27:26 +0800

    datetime.cpp add commit

--- datetime.cpp
+++ datetime.cpp
@@ -30,4 +30,9 @@ void DateTime::showTime()
 {
   printf("当前时间：%d/%d/%d %d:%d:%d\n", year, month, day, hour, minute, second);
 }
+void DateTime::showMoon()
+{
+	printf("当前时间：%d/%d/%d %d:%d:%d\n", year, month, day, hour, minute, second);
+	return 0;
+}
  
